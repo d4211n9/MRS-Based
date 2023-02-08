66c728959eef4484246b2428f410a9426fc46ecd
andreas14101 <andreasshansen@live.dk>
2023-02-08 12:10:41 +0100

    stuff v 2

--- src/dk/easv/presentation/controller/AppController.java
+++ src/dk/easv/presentation/controller/AppController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.FlowPane;
@@ -14,6 +15,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.File;
 import java.net.URL;
 import java.util.*;
 
@@ -68,7 +70,7 @@ public class AppController implements Initializable {
         model.loadData(model.getObsLoggedInUser());
         ObservableList<Movie> movieList = model.getObsTopMovieNotSeen();
         for (Movie m: model.getObsTopMovieSeen()) {
-            String titleAndRating = m.getTitle() + ", " + m.getAverageRating();
+            String titleAndRating = m.getTitle() + ", " + String.format("%.2f",m.getAverageRating());
 
             VBox newCard = createMovieVbox(titleAndRating);
             fpDisplay.getChildren().addAll(newCard);
@@ -79,23 +81,20 @@ public class AppController implements Initializable {
         //lvTopFromSimilar.setItems(model.getObsTopMoviesSimilarUsers());
 
 
-       // lvUsers.getSelectionModel().selectedItemProperty().addListener(
-             //   (observableValue, oldUser, selectedUser) -> {
-                  //  startTimer("Loading all data for user: " + selectedUser);
-                    //model.loadData(selectedUser);
-             //   });
-
         // Select the logged-in user in the listview, automagically trigger the listener above
         //lvUsers.getSelectionModel().select(model.getObsLoggedInUser());
     }
 
+
     private VBox createMovieVbox(String movie)
     {
+        File file = new File("C:/Users/andre/OneDrive/Skrivebord/unnamed.jpg");
+        Image image = new Image(file.toURI().toString());
         VBox mov = new VBox();
-        mov.getChildren().add(new ImageView());
+        mov.getChildren().add(new ImageView(image));
         mov.getChildren().add(new Label(movie));
-        mov.setSpacing(10);
-        mov.setMaxSize(50,50);
+        mov.setSpacing(100);
+
 
         return mov;
     }
